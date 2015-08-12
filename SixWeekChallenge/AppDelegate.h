122d4dcbdd4cb207da684a84e1debc5951c905d3
Scott <lordkappos@gmail.com>
2015-08-12 09:29:18 -0600

    Initial Commit

---
+++ SixWeekChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SixWeekChallenge
+//
+//  Created by Scott on 8/12/15.
+//  Copyright (c) 2015 Devmountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
